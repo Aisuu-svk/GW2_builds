21d895365f32aee4135940a3fad1242e7e2ec915
Aisuu <trelsf@gmail.com>
2022-03-25 20:45:06 +0100

    3

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(function () {
-  $("#menu").load("menu.html");
+  $("#menu").load("/html/menu.html");
 });
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
